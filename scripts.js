06f5942f8f2856c794c648b976ee8ba8bda501b4
Medvedev <alex-miha@tut.by>
2022-04-06 11:37:34 +0300

    Complited

---
+++ scripts.js
@@ -0,0 +1,158 @@
+// 2 задание
+
+let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг',
+    a6 = 785 * 653,
+    a7 = 100 / 25,
+    a8 = 0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5,
+    a14 = (9 - 12) * 7 / (5 + 2),
+    a15 = +"123",
+    a16 = 1 || 0,
+    a17 = false || true,
+    a18 = true > 0;
+
+//     console.log(a1);
+//     console.log(a2);
+//     console.log(a3);
+//     console.log(a4);
+//     console.log(a5);
+//     console.log(a6);
+//     console.log(a7);
+//     console.log(a8);
+//     console.log(a9);
+//     console.log(a10);
+//     console.log(a11);
+//     console.log(a12);
+//     console.log(a13);
+//     console.log(a14);
+//     console.log(a15);
+//     console.log(a16);
+//     console.log(a17);
+//     console.log(a18);
+
+//     console.log(typeof a1);
+//     console.log(typeof a2);
+//     console.log(typeof a3);
+//     console.log(typeof a4);
+//     console.log(typeof a5);
+//     console.log(typeof a6);
+//     console.log(typeof a7);
+//     console.log(typeof a8);
+//     console.log(typeof a9);
+//     console.log(typeof a10);
+//     console.log(typeof a11);
+//     console.log(typeof a12);
+//     console.log(typeof a13);
+//     console.log(typeof a14);
+//     console.log(typeof a15);
+//     console.log(typeof a16);
+//     console.log(typeof a17);
+//     console.log(typeof a18);
+
+// 3 задание
+
+// let width = 10,
+//     height = 23,
+//     SPryam = (width * height) + 'cm';
+
+// console.log(SPryam);
+
+// 4 задание
+// let h = 10,
+//     d = a7,
+//     VCilindra = 3.14 * (d / 2)^2 * h;
+
+// console.log(VCilindra);
+
+// 5 задание
+
+// let r = 5,
+//     SKruga = 3.14 * r^2;
+
+// console.log(SKruga);
+
+// 6 задание
+
+// let a = 5,
+//     b = 7,
+//     h = 10,
+//     STrap = ((a + b) / 2) * h;
+
+// console.log(STrap);
+
+// 7 задание
+
+// let S = 2000000,
+//     p = 0.10,
+//     years = 5,
+//     Pereplata = (S * p) * years;
+
+// console.log(Pereplata);
+
+// 8 задание
+
+// a+2(x-b)=16;
+// a + 2x - 2b = 16;
+// 2x = 16 + 2b - a;
+// x = (16 + 2b - a) / 2;
+
+// let a = 8,
+//     b = 3;
+// let x = (16 + 2*b - a) / 2;
+
+// console.log(x);
+
+// b(x+15)=a+6x;
+// bx + 15b = a + 6x;
+// bx + 15b - 6x = a;
+// (b - 6)x + 15b = a;
+// x = (a - 15b) / b - 6;
+
+// let x = (a - 15 * b) / (b - 6);
+
+// console.log(x);
+
+//x+2x+ax+bx=23780;
+// 3x + ax + bx = 23780;
+// x (3 + a + b) = 23780;
+// x = 23780 / 3 + a + b;
+
+// let x = 23780 / (3 + a + b);
+
+// console.log(x);
+
+// 9 задание
+
+// console.log(`Бывало, спит у ног собака,
+// костер занявшийся гудит,
+// и женщина из полумрака
+// глазами зыбкими глядит.
+
+// Потом под пихтою приляжет
+// на куртку рыжую мою
+// и мне, задумчивая, скажет:
+
+// "А ну-ка, спой!.."- и я пою. `);
+
+// 10 задание
+
+let str1 = 'индо земля зашаталась под ногами-и вырос, ',
+    str2 = 'и заревел он голосом диким… ',
+    str3 = 'блеснула молния и ударил гром, ',
+    str4 = 'а так какое-то чудище, страшное и мохнатое, ',
+    str5 = 'как будто из-под земли, перед купцом: ',
+    str6 = 'Он подошёл и сорвал аленький цветочек. ',
+    str7 = 'зверь не зверь, человек не человек, ',
+    str8 = 'В ту же минуту, безо всяких туч, ';
+
+let text = str6 + str8 + str3 + str1 + str5 + str7 + str4 + str2;
+
+console.log(text);
