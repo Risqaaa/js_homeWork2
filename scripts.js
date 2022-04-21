10976cd612fa136ee76cecd05bdec7f8342fed99
Medvedev <alex-miha@tut.by>
2022-04-21 16:51:11 +0300

    Fixed sleep, coffee

--- scripts.js
+++ scripts.js
@@ -5,21 +5,38 @@ const Fisherman = function() {
     let coffee = 0;
     let fish = 0;
     let result;
+    let resultGram;
+    let resultWeight;
     let stick = 1;
+    let stickInWater = false;
+    let sleep = false;
 
     this.throw = function() { // Забросить удочку
-        this.throw1();
-        setTimeout(this.throw2, 3200);
-    };
-
-    this.throw1 = function() {
-        console.log('------------------');
-        if(energy <= 0) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(energy <= 0) {
+            console.log('------------------');
             console.log('Нет сил, чтобы рыбачить!');
             console.log('Чтобы восстановить, нужно поспать или выпить чашку кофе.');
             return;
+        } else if(stickInWater == true) {
+            console.log('------------------');
+            console.log('Вы уже рыбачите в данный момент!');
+            return;
+        } else {
+            this.throw1();
+            setTimeout(this.throw2, 3200);
+            setTimeout(this.throw3, 3200);
         }
-        energy -= 10;
+    };
+
+    this.throw1 = function() {
+
+        stickInWater = true;
+        energy -= 5;
+        console.log('------------------');
         console.log('Оснастка заброшена в воду.');
         console.log('Поклёвка...');
 
@@ -36,27 +53,37 @@ const Fisherman = function() {
         weight += fish;
     };
 
+    this.throw3 = function() {
+        stickInWater = false;
+    };
+
     this.bite = function() { // Поклевка
         fish = Math.ceil(Math.random() * (1000 - 20) + 20);
-        console.log('Поймана рыба на ' + fish + ' грамм.');
+        console.log('------------------');
+        numGram();
+        console.log('Поймана рыба на ' + fish + ' ' + resultGram + '.');
     };
     
     this.bite2 = function() { // Поклевка на 2 удочку
         fish = Math.ceil(Math.random() * (3000 - 50) + 50);
-        console.log('Поймана рыба на ' + fish / 1000 + ' килограмм.');
+        console.log('------------------');
+        console.log('Поймана рыба на ' + fish / 1000 + ' ' + 'кило' + resultGram + '.');
     };
 
     this.bite3 = function() { // Поклевка на 3 удочку
         fish = Math.ceil(Math.random() * (7000 - 100) + 100);
-        console.log('Поймана рыба на ' + fish / 1000 + ' килограмм.');
+        console.log('------------------');
+        console.log('Поймана рыба на ' + fish / 1000 + ' ' + 'кило' + resultGram + '.');
     };
 
-    this.info = function() { // Статус
+    this.info = function() { // Информация
         console.log('------------------');
         if(weight > 999) {
-            console.log('Общий вес пойманной рыбы: ' + weight / 1000 + ' килограмм.');
+            numWeight();
+            console.log('Общий вес пойманной рыбы: ' + weight / 1000 + ' кило' + resultWeight + '.');
         } else {
-            console.log('Общий вес пойманной рыбы: ' + weight + ' грамм.');
+            numWeight();
+            console.log('Общий вес пойманной рыбы: ' + weight + ' ' + resultWeight + '.');
         }
 
         console.log('Деньги: ' + Math.round((money) * 100) / 100  + '$');
@@ -65,7 +92,7 @@ const Fisherman = function() {
         console.log('Кофе: ' + coffee + ' ' + result);
     };
 
-    const numCoffee = function(num = 0) { // Функуция правильного падежа
+    const numCoffee = function(num = 0) { // Функуция правильного падежа для кофе
         let words = ['стаканчик', 'стаканчика', 'стаканчиков'],
             count = coffee % 100;
 
@@ -87,16 +114,63 @@ const Fisherman = function() {
         return result;
     };
 
+    const numGram = function(num = 0) { // Функуция правильного падежа для граммов
+        let words = ['грамм', 'грамма', 'граммов'],
+            count = fish % 100;
+
+        if (count > 4 && count < 21) {
+            resultGram = words[2];
+        } else {
+            count = count % 10;
+            if (count == 1) {
+                resultGram = words[0];
+            } else {
+                if (count > 1 && count < 5) {
+                    resultGram = words[1];
+                } else {
+                    resultGram = words[2];
+                }
+            }
+        }
+
+        return resultGram;
+    };
+
+    const numWeight = function(num = 0) { // Функуция правильного падежа для граммов
+        let words = ['грамм', 'грамма', 'граммов'],
+            count = weight % 100;
+
+        if (count > 4 && count < 21) {
+            resultWeight = words[2];
+        } else {
+            count = count % 10;
+            if (count == 1) {
+                resultWeight = words[0];
+            } else {
+                if (count > 1 && count < 5) {
+                    resultWeight = words[1];
+                } else {
+                    resultWeight = words[2];
+                }
+            }
+        }
+
+        return resultWeight;
+    };
+
     this.sell = function() { // Продажа рыбы
-        console.log('------------------');
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(weight != 0) {
 
-        if(weight != 0) {
-
-            money = Math.round((money + weight / 2000) * 100) / 100;
+            money = Math.round((money + weight / 1000) * 100) / 100;
             console.log('Вы продали всю пойманную рыбу.');
-            console.log('Выручка: ' + Math.round((weight / 2000) * 100) / 100 + '$');
+            console.log('Выручка: ' + Math.round((weight / 1000) * 100) / 100 + '$');
             weight = 0;
         } else {
+            console.log('------------------');
             console.log('В инвентаре нет рыбы!');
         }
     };
@@ -105,8 +179,8 @@ const Fisherman = function() {
         energy = 100; 
     };
 
-    this.energy30 = function() { // Функция пополнения энергии
-        energy += 30;
+    this.energy70 = function() { // Функция пополнения энергии
+        energy += 70;
         if(energy > 100) {
             energy = 100;
         }
@@ -114,14 +188,20 @@ const Fisherman = function() {
 
     this.wake = function() {
         console.log('Вы выспались и полны энергии!');
+        sleep = false;
     };
 
     this.sleep = function() { // Сон
-        if(energy == 100) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        } else if(energy == 100) {
             console.log('------------------');
             console.log('Вы полны сил!');
             return;
         } else {
+            sleep = true;
             console.log('------------------');
             console.log('Вы легли спать.');
             console.log('*Ждем, пока вы проснетесь...*');
@@ -133,19 +213,27 @@ const Fisherman = function() {
     };
 
     this.buyCoffee = function() { // Купить кофе
-        if(money < 0.99) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(money < 1.99) {
             console.log('------------------');
             console.log('Недостаточно средств!');
         } else {
             console.log('------------------');
             console.log('Вы купили стаканчик кофе.');
-            money -= 0.99;
+            money -= 1.99;
             coffee += 1;
         }
     };
 
     this.drinkCoffee = function() { // Выпить кофе
-        if(coffee <= 0) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(coffee <= 0) {
             console.log('------------------');
             console.log('В инвентаре нет кофе!');
             return;
@@ -153,12 +241,16 @@ const Fisherman = function() {
             console.log('------------------');
             console.log('Вы выпили стаканчик кофе.');
             coffee -= 1;
-            this.energy30();
+            this.energy70();
         }
     };
 
     this.buyStick2 = function() { // Покупка удочки 2
-        if(stick == 2 || stick == 3) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(stick == 2 || stick == 3) {
             console.log('------------------');
             console.log('У вас уже есть эта удочка!');
             return;
@@ -174,7 +266,11 @@ const Fisherman = function() {
     };
 
     this.buyStick3 = function() { // Покупка удочки 3 
-        if(stick == 3) {
+        if(sleep == true) {
+            console.log('------------------');
+            console.log('В данный момент вы отдыхаете...');
+            return;
+        }else if(stick == 3) {
             console.log('------------------');
             console.log('У вас уже есть эта удочка!');
             return;
@@ -195,11 +291,14 @@ const Fisherman = function() {
 let fisherman = new Fisherman();
 
 document.write('<h1>Белорусская Рыбалка :) (0.00001 ver.)</h1>');
-document.write('fisherman.info() - основная информация;' + '<br>');
-document.write('fisherman.throw() - забросить оснастку в воду;' + '<br>');
+document.write('------------------ Основное' + ' ----------------------------' + '<br>');
+document.write('fisherman.info() - основная информация' + '<br>');
+document.write('fisherman.throw() - забросить оснастку в воду' + '<br>');
 document.write('fisherman.sell() - продать всю пойманную рыбу' + '<br>');
+document.write('------------------ Восполнение энергии' + ' -------------' + '<br>');
 document.write('fisherman.sleep() - лечь спать' + '<br>');
-document.write('fisherman.buyCoffee() - купить стакан кофе (цена: 0.99$)' + '<br>');
 document.write('fisherman.drinkCoffee() - выпить стакан кофе' + '<br>');
+document.write('------------------ Магазин' + ' ------------------------------' + '<br>');
+document.write('fisherman.buyCoffee() - купить стакан кофе (цена: 1.99$)' + '<br>');
 document.write('fisherman.buyStick2() - купить удочку TeleStick 4000 за 100$ (2 уровень)' + '<br>');
 document.write('fisherman.buyStick3() - купить удочку SuperCatcher Montana 3 за 500$ (3 уровень)' + '<br>');
